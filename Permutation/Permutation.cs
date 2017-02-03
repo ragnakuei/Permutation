5f4a6ad1f1f62f2a5d27570fde8640361014b5d4
DESKTOP-K48D6IA\Kuei <ragnakuei@livemail.tw>
2017-02-03 22:56:29 +0800

    complete

---
+++ Permutation/Permutation.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Permutation
+{
+    public class Permutation
+    {
+        public List<List<string>> GetResult(List<string> source)
+        {
+            this._count = source.Count;
+            InitialIndexPermutation();
+
+            while (_endFlag == false)
+            {
+                ResultAdd(source);
+            }
+
+            return _result;
+        }
+
+        private void ResultAdd(List<string> source)
+        {
+            _indexPermutation.Reverse();
+            List<int> cloneIndex = Clone(_indexPermutation);
+            _indexPermutation.Reverse();
+
+            List<string> tmpResult = new List<string>();
+            List<string> sourceData = Clone(source);
+            for (int i = 0; i < this._count; i++)
+            {
+                tmpResult.Add(sourceData[cloneIndex[0]]);
+
+                sourceData.RemoveAt(cloneIndex[0]);
+                cloneIndex.RemoveAt(0);
+            }
+
+            _result.Add(tmpResult);
+
+            _indexPermutation[1]++;
+            CarryBit();
+        }
+
+        private List<T> Clone<T>(List<T> source)
+        {
+            List<T> clone = new List<T>();
+            source.ForEach(i => clone.Add(i));
+            return clone;
+        }
+
+        private void InitialIndexPermutation()
+        {
+            foreach (var i in Enumerable.Range(0, _count))
+            {
+                _indexPermutation.Add(0);
+            }
+        }
+
+        private void CarryBit()
+        {
+            for (int i = 1; i < _indexPermutation.Count; i++)
+            {
+                if (_indexPermutation[i] > i)
+                {
+                    if (i == _indexPermutation.Count - 1)
+                    {
+                        _endFlag = true;
+                        return;
+                    }
+                    _indexPermutation[i + 1]++;
+                    _indexPermutation[i] = 0;
+                }
+            }
+        }
+
+        private List<List<string>> _result = new List<List<string>>();
+
+        private int _count = 0;
+
+        private List<int> _indexPermutation = new List<int>();
+
+        private bool _endFlag = false;
+    }
+
+}
---
+++ Permutation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> source = new List<string>{ "1","2","3","4"};
+            Permutation target = new Permutation();
+            List<List<string>> result = target.GetResult(source);
+
+            foreach (var item in result)
+            {
+                foreach (var element in item)
+                {
+                    Console.Write(element+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
